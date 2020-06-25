9cbb7c05feaf3dd093115ec9fcb7f8a761c373e1
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-25 14:43:14 +0530

    created user schema

--- models/user.js
+++ models/user.js
@@ -1,1 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+var userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+    maxlength: 32,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  bio: {
+    type: String,
+    trim: true,
+  },
+  // TODO: password
+  password: {
+    type: String,
+    trim: true,
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0,
+  },
+  userPosts: {
+    type: Array,
+    default: [],
+  },
+});
