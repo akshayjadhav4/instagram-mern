5576d21b4c0f2babfc374d1a51c8b8223b60c1ca
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-25 16:22:52 +0530

    added mongo db connection string

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express()
 
-mongoose.connect('mongodb://localhost:27017/instagram', {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/instagram", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 const port = 2004
 
