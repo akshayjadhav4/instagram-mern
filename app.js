2831b668d4f10a96be5db7abeebc31687ad39913
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-25 16:17:50 +0530

    added port and listen

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express()
 
+const port = 2004
 
+app.listen(port,()=>{
+    console.log(`Server running at ${port}`);
+    
+})
