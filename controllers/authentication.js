cec9aa0f9e08345a09d83db3a6831d6b5aee354b
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-25 18:06:43 +0530

    signup function added

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,6 +1,14 @@
+const User = require("../models/user")
 
-exports.signOut = (req,res)=>{
-    res.json({
-        message :"User SignOut"
+exports.signup = (req ,res)=>{
+    const user = new User(req.body)
+    user.save((error , user)=>{
+        if (error) {
+            return res.status(400).json({
+                error : "Not able to create account"
+            })
+        }
+        res.json(user)
     })
+
 }
