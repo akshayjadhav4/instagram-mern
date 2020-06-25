8b3bef66a57a4af1e578e51dbc52faba57b84002
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-25 18:50:25 +0530

    validation added

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,9 +1,22 @@
 var express = require('express')
 var router = express.Router()
+const { check } = require('express-validator');
 
 const {signup} = require("../controllers/authentication")
 
-router.post("/signup",signup)
+router.post(
+  "/signup",
+  [
+    check("fullname")
+      .isLength({ min: 5 })
+      .withMessage("must be at least 5 chars long"),
+    check("password")
+      .isLength({ min: 5 })
+      .withMessage("must be at least 5 chars long"),
+    check("email").isEmail().withMessage("provide valid email"),
+  ],
+  signup
+);
 
 
 
