bd0bb988e431d268e114ccf05441fff665814b1b
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-25 15:56:33 +0530

    post schema created

--- models/post.js
+++ models/post.js
@@ -1,5 +1,32 @@
 const mongoose = require("mongoose");
-
+const { ObjectId } = mongoose.Schema;
 const postSchema = new mongoose.Schema({
-    
-})
+  description: {
+    type: String,
+    trim: true,
+    maxlength: 200,
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 25,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  author: {
+    type: ObjectId,
+    ref: "User",
+  },
+  comments: {
+    type: Array,
+    default: [],
+  },
+  photo: {
+    required:true,
+    data: Buffer,
+    contentType: String,
+  },
+});
+module.exports = mongoose.model("Post", postSchema);
