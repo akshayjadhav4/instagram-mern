ea45f7c93371d069dcb1c7eea0181ee1e2102985
AKSHAY <akshayjadhav461999@gmail.com>
2020-06-26 16:43:16 +0530

    signin functionality with jwt token added

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,6 @@
 const User = require("../models/user")
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 const { validationResult  } = require('express-validator');
 
 exports.signup = (req ,res)=>{
@@ -21,3 +23,37 @@ exports.signup = (req ,res)=>{
         res.json(user)
     })
 }
+
+
+exports.signin = (req, res) => {
+  const errors = validationResult(req);
+  const { email, password } = req.body;
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+      param: errors.array()[0].param,
+    });
+  }
+
+  User.findOne({ email }, (error, user) => {
+    if (error || !user) {
+      return res.status(400).json({
+        error: "User account does not exists...",
+      });
+    }
+
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and Password do not match",
+      });
+    }
+
+    //CREATE TOKEN
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+    // PUT TOKEN IN COOKIE
+    res.cookie("token", token, { expire: new Date() + 9999 });
+    //SEND RESPONSE TO FRONTEND
+    const { _id, fullname, username, email, role } = user;
+    return res.json({ token, user: { _id, fullname, username, email, role } });
+  });
+};
--- routes/authentication.js
+++ routes/authentication.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 const { check } = require('express-validator');
 
-const {signup} = require("../controllers/authentication")
+const {signup,signin} = require("../controllers/authentication")
 
 router.post(
   "/signup",
@@ -16,6 +16,17 @@ router.post(
     check("email").isEmail().withMessage("provide valid email"),
   ],
   signup
+);
+
+router.post(
+  "/signin",
+  [
+    check("password")
+      .isLength({ min: 5 })
+      .withMessage("must be at least 5 chars long"),
+    check("email").isEmail().withMessage("provide valid email"),
+  ],
+  signin
 );
 
 
