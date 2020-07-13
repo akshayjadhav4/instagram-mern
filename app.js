a1f66497f32c5ad338ac9c8e325e6e81919c0bc2
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-13 21:25:43 +0530

    socket.io setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const mongoose = require("mongoose");
 const express = require("express");
+const client = require("socket.io").listen(1204).sockets; //running socket.io
 const app = express()
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -10,6 +11,7 @@ const authRoutes = require("./routes/authentication")
 const userRoutes = require("./routes/user")
 const postRoutes = require("./routes/post")
 const commentRoutes = require("./routes/comment")
+const Message = require("./models/message")
 
 mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true,
@@ -17,6 +19,58 @@ mongoose.connect(process.env.DATABASE, {
   useCreateIndex: true,
 }).then(()=>{
     console.log("DB CONNECTED");
+
+    //connect to socket.io
+    client.on("connection", function (socket) {
+      // Get the last messages from the database.
+
+      socket.on('join',chatSession=>{
+        Message.find(
+          // { chatSession: chatSession }
+          {$or : [{chatSession:chatSession.chatSession1},{chatSession:chatSession.chatSession2}]}
+           )
+        .sort({ createdAt: -1 })
+        .limit(20)
+        .exec((err, messages) => {
+          if (err) return console.error(err);
+
+          // Send the last messages to the user.
+          socket.emit("output", messages);
+        });  
+      })
+
+
+      //Listen to connected users for a new message.
+      socket.on("message", function (messageInput) {
+        // Create a message with the content and the name of the user.
+        const message = new Message({
+          content: messageInput.content,
+          user: messageInput.user,
+          chatSession : messageInput.chatSession
+        });
+
+      
+        
+        if (messageInput.content == "" || messageInput.user == "") {
+          socket.emit("status", "Please provide content and user");
+        } else {
+          // Save the message to the database.
+          message.save((err,msg) => {
+            if (err) return console.error(err);
+          });
+
+          socket.emit("push", messageInput);
+        }
+      });
+
+      //handle clear
+      socket.on("clear", function (data) {
+        //remove all chats from database
+        Message.remove({}, function () {
+          socket.emit("cleared");
+        });
+      });
+    });
     
 })
 
